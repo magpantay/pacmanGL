80e5f8f5d69ca0a6a2c69d7eda0a4e2b184df775
Kyran Magpantay <magpantay.kyran@gmail.com>
2018-05-01 15:40:36 -0700

    Added .h files in the includes

--- App.h
+++ App.h
@@ -5,6 +5,11 @@
 #include "TexRect.h"
 #include "AnimatedRect.h"
 
+#include "board.h"
+//#include "ghosts.h"
+//#include "pacman.h"
+#include "populatePellets.h"
+
 class App: public GlutApp {
     // Maintain app state here
     float mx;
@@ -12,36 +17,36 @@ class App: public GlutApp {
 public:
     // Constructor, to initialize state
     App(const char* label, int x, int y, int w, int h);
-    
+
     // These are the events we want to handle
     void draw();
     void keyPress(unsigned char key);
     void mouseDown(float x, float y);
     void mouseDrag(float x, float y);
-    
+
     void specialKeyPress(int key);
-    
+
     void specialKeyUp(int key);
-    
+
     void idle();
-    
+
     TexRect* ball;
-    
+
     TexRect* platform;
-    
+
     TexRect* background;
-    
+
     AnimatedRect* gameOver;
-    
+
     bool up;
     bool down;
     bool left;
     bool right;
-    
+
     bool moving;
     bool game_over;
-    
-    
+
+
 };
 
 #endif
