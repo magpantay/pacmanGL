cf13711872b34bcbd645ae434164bba350e45019
Kyran Magpantay <magpantay.kyran@gmail.com>
2018-05-05 18:01:12 -0700

    Revert "How did that semicolon get there?"
    This reverts commit 52b874698995c677b1b5ee1990f25e62a0a1d5b7.

--- pacman.cpp
+++ pacman.cpp
@@ -1,6 +1,6 @@
 #include "pacman.h"
 
-pacman::pacman()
+pacman::pacman();
 {
     pac = new AnimatedRect("BMPs/pacman/pacman_left.png", 1, 4, -1.0+9.0*(2.0/19.0), -0.2, (2.0/19.0), 0.1);
     left = true; //initialize to true, we want pacman facing left
